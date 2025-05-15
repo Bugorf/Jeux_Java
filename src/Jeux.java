9ca5d58bec45c2ef8e629a9637901c7f752c6185
Bugorf <45924820+Bugorf@users.noreply.github.com>
2025-05-15 20:47:01 +0200

    La version finale

---
+++ src/Jeux.java
@@ -0,0 +1,13 @@
+package src;
+
+import src.controleur.ControleurJeu;
+
+import java.io.IOException;
+
+public class Jeux {
+
+    public static void main(String[] args) throws IOException {
+        new ControleurJeu();
+    }
+
+}
